4a70bfd5ca76174e6c85a3883b7e3a1d4e57f8db
Link <mexlove2@gmail.com>
2025-04-21 17:02:57 +0900

    replace to stomp

---
+++ src/main/java/chat_service/controller/StompChatController.java
@@ -0,0 +1,21 @@
+package chat_service.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class StompChatController {
+
+  @MessageMapping("/chats")
+  @SendTo("/sub/chats")
+  public String handleMessage(@Payload String message) {
+    log.info("{} received", message);
+
+    return message;
+  }
+
+}
